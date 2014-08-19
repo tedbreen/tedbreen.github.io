94ee6dd3c0946374c6d95fb14aa4aeeb663c74f3
Edward Breen <ted.breen@gmail.com>
2014-08-19 08:24:37 -0400

    first commit

---
+++ navbar.js
@@ -0,0 +1,8 @@
+var navOne = document.getElementById('nav-1');
+var navTwo = document.getElementById('nav-2');
+var navThree = document.getElementById('nav-3');
+var navFour = document.getElementById('nav-4');
+navOne.addEventListener('click', function () {alert('HOME');}, false);
+navTwo.addEventListener('click', function () {alert('ABOUT');}, false);
+navThree.addEventListener('click', function () {alert('PORTFOLIO');}, false);
+navFour.addEventListener('click', function () {alert('CONTACT');}, false);
