a9785c1dfc0a602665e970126dcd19e32b0c9951
Edward Breen <ted.breen@gmail.com>
2014-08-23 18:26:27 -0400

    replace divs when click nav bar divs

--- navbar.js
+++ navbar.js
@@ -1,22 +1,38 @@
-// var navOne = document.getElementById('nav-1');
-// var navTwo = document.getElementById('nav-2');
-// var navThree = document.getElementById('nav-3');
-// var navFour = document.getElementById('nav-4');
-// navOne.addEventListener('click', function () {alert('HOME');}, false);
-// navTwo.addEventListener('click', function () {alert('ABOUT');}, false);
-// navThree.addEventListener('click', function () {alert('PORTFOLIO');}, false);
-// navFour.addEventListener('click', function () {alert('CONTACT');}, false);
+var divNav1 = document.getElementById("nav-1");
+var divNav2 = document.getElementById("nav-2");
+var divNav3 = document.getElementById("nav-3");
+var divNav4 = document.getElementById("nav-4");
 
+var divHome = document.getElementById("div-home");
+var parentDiv = divHome.parentNode;
+var divAbout = document.createElement("div");
+divAbout.setAttribute("id", "div-about");
+var divPortfolio = document.createElement("div");
+divPortfolio.setAttribute("id", "div-portfolio");
+var divContact = document.createElement("div");
+divContact.setAttribute("id", "div-contact");
 
-var els = document.getElementsByClassName('nav-el');
+var divHomeContent = divHome.innerText;
+var divAboutContent = document.createTextNode("My bio here");
+var divPortfolioContent = document.createTextNode("Look at these");
+var divContactContent = document.createTextNode("Call me babe");
 
-for (var i = 0; i < els.length; i++) {
-  els[i].addEventListener('mouseenter', function () {
+var navDivs = [divNav1, divNav2, divNav3, divNav4];
+var contents = [
+  divHomeContent, divAboutContent, divPortfolioContent, divContactContent
+];
+
+var colors = function(el) {
+  el.addEventListener('mouseenter', function () {
     this.style.color = "yellow";
-    // this.style.fontSize = "1.85em";
   }, false);
-  els[i].addEventListener('mouseleave', function () {
+  el.addEventListener('mouseleave', function () {
     this.style.color = "white";
-    // this.style.fontSize = "1.75em";
   }, false);
-}
+  // trying to add click event to change divs
+  // el.addEventListener('click', function() {
+  //   this.appendChild(contents[i]);
+  // }, false);
+};
+
+for (var i = 0; i < navDivs.length; i++) { colors(navDivs[i]) }
