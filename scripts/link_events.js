76df3b86303773cb36e685a8a42bf1b6d6700cec
Edward Breen <ted.breen@gmail.com>
2014-10-03 01:03:14 -0700

    add content to pages

--- scripts/link_events.js
+++ scripts/link_events.js
@@ -1,7 +1,22 @@
-//home link
+function changeDiv(link, div) {
+  link.addEventListener("click", function() {
+    event.preventDefault();
+    currentDiv.style.display = "none";
+    div.style.display = "block";
+    currentDiv = div;
+  });
+}
 
-//about link
+var homeDiv = document.getElementById("home");
+var homeLink = document.getElementById("home-link");
+var aboutDiv = document.getElementById("about");
+var aboutLink = document.getElementById("about-link");
+var portfolioDiv = document.getElementById("portfolio");
+var portfolioLink = document.getElementById("portfolio-link");
+var contactDiv = document.getElementById("contact");
+var contactLink = document.getElementById("contact-link");
 
-//portfolio link
-
-//contact link
+changeDiv( aboutLink, aboutDiv );
+changeDiv( contactLink, contactDiv );
+changeDiv( portfolioLink, portfolioDiv );
+changeDiv( homeLink, homeDiv );
