b4d61cc2f3f9fb426f11e3a054d60002b54f56ba
Edward Breen <ted.breen@gmail.com>
2014-10-03 13:56:32 -0700

    format About page, resize avatar

--- scripts/link_events.js
+++ scripts/link_events.js
@@ -1,5 +1,5 @@
 function changeDiv(link, div) {
-  link.addEventListener("click", function() {
+  link.addEventListener("click", function(event) {
     event.preventDefault();
     currentDiv.style.display = "none";
     div.style.display = "block";
