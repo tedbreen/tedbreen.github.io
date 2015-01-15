64966e3769ae4e0fd3b916dfafeaa39a32074269
Edward Breen <ted.breen@gmail.com>
2015-01-15 15:44:18 -0800

    removed deprecated changeDiv function

--- js/link_events.js
+++ js/link_events.js
@@ -1,12 +1,3 @@
-function changeDiv(link, div) {
-  link.addEventListener("click", function(event) {
-    event.preventDefault();
-    currentDiv.style.display = "none";
-    div.style.display = "block";
-    currentDiv = div;
-  });
-}
-
 function highlightTitle(div, link) {
   jQuery( div ).hover(
     function() { link.style.color = "yellow" },
