1870da1cca4d9f4bea36dae19da2c6c49fd90d72
Edward Breen <ted.breen@gmail.com>
2015-01-15 16:18:02 -0800

    added smooth scrolling to link nav

--- js/doc_ready.js
+++ js/doc_ready.js
@@ -5,8 +5,27 @@
         function() { link.style.color = "black" }
       );
     }
-    
+
+    function scrollEvent(divName, number) {
+        jQuery('a[href=#' + divName + ']').click(function(event) {
+            event.preventDefault();
+            jQuery("body").animate({ scrollTop: number });
+        });
+    }
+
+    function getOffset(divName) {
+        var value = jQuery("#" + divName).offset().top;
+        return value;
+    }
+
     jQuery( document ).ready( function() {
+        // enable smooth scrolling for link navigation
+        ["about", "portfolio", "contact"].forEach(function(divName) {
+            var divOffset = getOffset(divName);
+            scrollEvent( divName, divOffset );
+        });
+        scrollEvent( "", 0 );
+
         // following lines enable highlighting on portfolio links
         highlightTitle( "#running", jQuery("#running-link")[0] );
         highlightTitle( jQuery("#running-link")[0], jQuery("#running-link")[0] );
