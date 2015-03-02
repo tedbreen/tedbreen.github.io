c895e6b9b4a752a5e639ca1b8eac029e2d6dd12f
Edward Breen <ted.breen@gmail.com>
2015-03-01 22:03:34 -0800

    updated content and styling

--- js/doc_ready.js
+++ js/doc_ready.js
@@ -20,20 +20,8 @@
 
     jQuery( document ).ready( function() {
         // enable smooth scrolling for link navigation
-        ["about", "portfolio", "contact"].forEach(function(divName) {
-            var divOffset = getOffset(divName);
-            scrollEvent( divName, divOffset );
-        });
+        var divOffset = getOffset('contact');
+        scrollEvent('contact', divOffset);
         scrollEvent( "", 0 );
-
-        // following lines enable highlighting on portfolio links
-        highlightTitle( "#running", jQuery("#running-link")[0] );
-        highlightTitle( jQuery("#running-link")[0], jQuery("#running-link")[0] );
-        highlightTitle( "#earthquake", jQuery("#earthquake-link")[0] );
-        highlightTitle( jQuery("#earthquake-link")[0], jQuery("#earthquake-link")[0] );
-        highlightTitle( "#slots", jQuery("#slots-link")[0] );
-        highlightTitle( jQuery("#slots-link")[0], jQuery("#slots-link")[0] );
-        highlightTitle( "#doc-analyzer", jQuery("#doc-analyzer-link")[0] );
-        highlightTitle( jQuery("#slots-link")[0], jQuery("#doc-analyzer-link")[0] );
     });
 })();
